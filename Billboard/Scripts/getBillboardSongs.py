292f1514d91379fa765e7434115b9da85cd91176
Pratyush Shah <89347300+pratyushvshah@users.noreply.github.com>
2022-07-31 15:51:17 -0400

    cleaned the billboard script for fetching songs.

--- Billboard/Scripts/getBillboardSongs.py
+++ Billboard/Scripts/getBillboardSongs.py
@@ -7,31 +7,19 @@ import billboard
 import datetime
 import json
 
-# Dates for 2010s
-decade = 2000
+
+decade = 2010
 cutOffDate = datetime.date(2010, 1,1)
 currentDate = datetime.date(2020,1,1)
-
-# Dates for 2000s
-# decade = 2010
-# cutOffDate = datetime.date(2000, 1,1)
-# currentDate = datetime.date(2010,1,1)
-
 chart = billboard.ChartData("hot-100", date=currentDate)
 
 songs = []
 
-def getSongs():
-    for song in chart:
-        songs.append(song.title + " by " + song.artist)
-
 while currentDate > cutOffDate:
-    getSongs()
-    prevDate = currentDate - datetime.timedelta(days=7)
-
-    chart = billboard.ChartData("hot-100", prevDate)
-    currentDate = prevDate
-    
+    chart = billboard.ChartData("hot-100", enddate)
+    for song in chart:
+        songs.append(song.title + " - " + song.artist)
+    enddate = enddate - datetime.timedelta(days = 7)
 
 with open(f"BillboardWithDuplicates{decade}.json", "w") as file:
     json.dump(songs,file,indent=2)
